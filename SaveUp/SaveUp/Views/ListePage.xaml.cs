64d654411ac51e48455fcae15d59a3e1d9d5894d
Luxioo <88648923+Luxioo@users.noreply.github.com>
2022-04-25 16:23:25 +0200

    Bevor MVVM

--- SaveUp/SaveUp/Views/ListePage.xaml.cs
+++ SaveUp/SaveUp/Views/ListePage.xaml.cs
@@ -13,15 +13,18 @@ namespace SaveUp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListePage : ContentPage
     {
-        public ListePage(List<item> itemliste)
+        public List<item> Itemliste = new List<item>();
+        public ListePage(List<item> dieitemlistepara)
         {
             string x = "";
             InitializeComponent();
-            foreach (var item in itemliste)
+            foreach (var item in dieitemlistepara)
             {
                 x += $" {item.name}  {item.betrag}  {item.uhrzeit} \n";
             }
             ed_te.Text = x;
+
+            Itemliste = dieitemlistepara;
         }
 
         private void bt_sp_Clicked(object sender, EventArgs e)
@@ -40,15 +43,33 @@ namespace SaveUp
             }
             catch (Exception)
             {
-
-                throw;
+                SimpleAlert("Fehler", "Etwas ist schiefgelaufen!", "OK");
             }
+        }
+
+        /// <summary>
+        /// Makes an Simple Alert, i had to do that because i wanted to have Displays and they need an async Task
+        /// </summary>
+        /// <param name="Titel"></param>
+        /// <param name="Text"></param>
+        /// <param name="Buttontext"></param>
+        /// <returns></returns>
+        private async Task SimpleAlert(string Titel, string Text, string Buttontext)
+        {
+            await DisplayAlert(Titel, Text, Buttontext);
         }
 
         private void bt_ei_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new MainPage());
 
+        }
+
+        private void bt_le_Clicked(object sender, EventArgs e)
+        {
+            /*
+            Itemliste.Clear();
+            */
         }
     }
 }
